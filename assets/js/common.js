216e7b470791b7250b99055300a512c640bba5b2
Fpsska <fpsska1337@gmail.com>
2021-06-30 12:53:43 +0300

    refactor: replace svg icons for footer & burger sections

--- assets/js/common.js
+++ assets/js/common.js
@@ -37,19 +37,44 @@ document.addEventListener("DOMContentLoaded", () => {
     })
 
     // /.burger menu
+
+    let validationFunction = () => {
+        'use strict';
+        window.addEventListener('load', () => {
+            let form = document.getElementById('needs-validation');
+            form.addEventListener('submit', (event) => {
+                if (form.checkValidity() === false) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                form.classList.add('was-validated');
+            }, false);
+        }, false);
+    }
+    
+    validationFunction();
+    
+    // /.validation
 });
 
-(function () {
-    'use strict';
-    window.addEventListener('load', function () {
-        var form = document.getElementById('needs-validation');
-        form.addEventListener('submit', function (event) {
-            if (form.checkValidity() === false) {
-                event.preventDefault();
-                event.stopPropagation();
-            }
-            form.classList.add('was-validated');
-        }, false);
-    }, false);
-})();
-// /.validation
+
+
+
+
+
+
+
+// (function () {
+//     'use strict';
+//     window.addEventListener('load', function () {
+//         let form = document.getElementById('needs-validation');
+//         form.addEventListener('submit', function (event) {
+//             if (form.checkValidity() === false) {
+//                 event.preventDefault();
+//                 event.stopPropagation();
+//             }
+//             form.classList.add('was-validated');
+//         }, false);
+//     }, false);
+// })();
+
